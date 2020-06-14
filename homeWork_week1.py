890ac71d2ff1bc95b15e7f3ed32e14b2a2333850
hahaXing <pengxingl@sina.com>
2020-06-14 23:23:00 +0800

    week1-1 2.0

--- homeWork_week1.py
+++ homeWork_week1.py
@@ -1,3 +1,4 @@
+#删除重复数字
 def removeDuplicates(nums):
     if not nums:
         return 0
@@ -8,9 +9,47 @@ def removeDuplicates(nums):
             nums[i] = nums[j]
     return i + 1
 
+#数组平移K位
+def rotate(nums, k):
+        n = len(nums)
+        k %= n
+        nums[:] = nums[-k:] + nums[:-k]
+
+        print(nums)
+
+#找出和为target的两个数的下标
+def twoSum(nums,target):
+    d = {}
+    n = len(nums)
+    for x in range(n):
+        if target - nums[x] in d:
+            return d[target-nums[x]],x
+        else:
+            d[nums[x]] = x
+
+#将数字0移到数组末尾
+def moveZeroes(nums):
+    if not nums:
+        return 0
+    j = 0
+    for i in range(len(nums)):
+        if nums[i]:
+            nums[j],nums[i] = nums[i],nums[j]
+            j += 1
+
+    print(nums)
+
 
 if __name__ == '__main__':
-    nums = [5, 8, 7, 9, 3, 4, 8, 3, 2, 1, 6, 7, 4, 8, 5, 9, 3, 2, 1, 5, 7, 4, 6, 9, 8]
-    nums.sort()
-    length = removeDuplicates(nums)
-    print(length)
+    nums = [5, 0, 7, 9, 0, 4, 8, 3, 2, 0, 6, 7, 4, 8, 0, 9, 3, 2, 1, 5, 0, 4, 6, 9, 8]
+
+    #nums.sort()
+    #length = removeDuplicates(nums)
+    #print(length)
+
+    #rotate(nums, 3)
+
+    #x, y = twoSum(nums,8)
+    #print(x, y)
+
+    moveZeroes(nums)
